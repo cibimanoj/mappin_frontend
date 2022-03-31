b6832c861c4bbdd77dd31a8dc0a9712ef3854774
cibimanoj <86293164+cibimanoj@users.noreply.github.com>
2022-03-31 20:39:32 +0530

    changes

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                       presets: ['my-custom-babel-preset'],
-                      ignore: [ './node_modules/mapbox-gl/dist/mapbox-gl.js' ]
                     }
                   }
             }
